a161d032e6cad7e7d0737eb4d36382595ec1c607
pietro776 <pietrojananni@gmail.com>
2025-04-24 17:27:08 -0300

    atualizacao

--- src/main/java/org/example/services/FormaPagamentoService.java
+++ src/main/java/org/example/services/FormaPagamentoService.java
@@ -1,6 +1,40 @@
 package org.example.services;
 
-public class FormaPagamentoService {
-    
+import antlr.collections.List;
+import org.example.entities.FormaPagamento;
+import org.example.repositories.FormaPagamentoRepository;
+import org.example.services.exeptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Optional;
+
+public class FormaPagamentoService {
+    @Autowired
+    private FormaPagamentoRepository repository;
+
+    public List<FormaPagamento> getAll() {
+        return repository.findAll();
+    }
+    public FormaPagamento findById(long id) {
+        Optional<FormaPagamento> optionalFormaPagamento = repository.findById(id);
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+    public FormaPagamento insert(FormaPagamento formapagamento) {
+        return repository.save(FormaPagamento);
+    }
+
+    public boolean update(Long id, FormaPagamento formapagamento) {
+        Optional<FormaPagamento> optionalFormaPagamento = repository.findById(id);
+        if (optionalFormaPagamento.isPresent()) {
+         FormaPagamento formaPagamento = optionalFormaPagamento.get();
+         formaPagamento.setFpgDescricao(formaPagamento.getFpgDescricao());
+         repository.save(formaPagamento);
+         return true;
+        }
+    }
+    return false;
 }
+public void delete(long id) {
+    repository.deleteById(id);
+}
+
--- src/main/java/org/example/services/ProdutoService.java
+++ src/main/java/org/example/services/ProdutoService.java
@@ -13,7 +13,6 @@ import java.util.Optional;
 
 @Service
 public class ProdutoService {
-
     @Autowired
     private ProdutoRepository repository;
 
